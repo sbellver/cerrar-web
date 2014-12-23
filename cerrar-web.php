103294d29ca865ff3b79f0580b7d07cc8120c60a
Sergio Bellver Aliaga <sbellver@tecneca.com>
2014-12-23 18:16:32 +0100

    Mejoras para la publicación en Wordpress

--- cerrar-web.php
+++ cerrar-web.php
@@ -30,9 +30,10 @@ License: GPL2
 
 function tec_plugin_web_cerrada() {
 
+    $plugindir = plugins_url();
     $title = get_bloginfo('name');
     $mantenimiento = 'En estos momentos estamos renovando la web, disculpen las molestias';
-    $pie = ' Copyright © <a href="http://tecneca.com" target="_blank" rel="nofollow" style="color: #fff">Tecneca Networks, S.L.</a> ';
+    $pie = ' Copyright © '.$title;
 
 	if(!is_user_logged_in()){
 			echo '
@@ -47,16 +48,9 @@ function tec_plugin_web_cerrada() {
     <meta name="description" content="">
     <meta name="author" content="sbellver at Tecneca .com">
     <title>'.$title.'</title>
-   <link href="wp-content/plugins/cerrar-web/close/css/bootstrap.min.css" rel="stylesheet">
-   <link href="wp-content/plugins/cerrar-web/close/css/freelancer.css" rel="stylesheet">
-    <link href="wp-content/plugins/cerrar-web/close/font-awesome-4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
-    <link href="http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
-
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
+   <link href="'.$plugindir.'/cerrar-web/close/css/bootstrap.min.css" rel="stylesheet">
+   <link href="'.$plugindir.'/cerrar-web/close/css/freelancer.css" rel="stylesheet">
+   <link href="'.$plugindir.'/cerrar-web/close/font-awesome-4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
 </head>
 
@@ -68,7 +62,7 @@ function tec_plugin_web_cerrada() {
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
-                    <img class="img-responsive" src="wp-content/plugins/cerrar-web/close/img/profile.png" alt="">
+                    <img class="img-responsive" src="'.$plugindir.'/cerrar-web/close/img/profile.png" alt="">
                     <div class="intro-text">
                         <span class="name">'.$title.'</span>
                         <hr class="star-light">
